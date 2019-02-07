5fb535a64fb3d63e9a3fdd6ce832555823516273
Angelichorsey <>
2019-02-07 12:30:44 -0600

    removed psuedocode line

--- course_6_0001/problem-sets/ps3/ps3.py
+++ course_6_0001/problem-sets/ps3/ps3.py
@@ -260,7 +260,6 @@ def play_hand(hand, word_list):
       
     """
     
-    # BEGIN PSEUDOCODE <-- Remove this comment when you implement this function
     # Keep track of the total score
     total_score = 0
     # As long as there are still letters left in the hand:
